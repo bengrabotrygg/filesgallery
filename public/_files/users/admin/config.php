e80d31947428d2735949c8f0ffafb321f756e77d
Benjamin Graf <benjamin.graf@botrygg.se>
2026-06-12 13:48:12 +0200

    base config

---
+++ public/_files/users/admin/config.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+  'password' => 'changeme',
+  'allow_all' => true,
+  'allow_settings' => false,
+  'allow_tests' => false,
+];
---
+++ public/_files/users/superadmin/config.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+  'password' => 'changeme',
+  'allow_all' => true,
+  'allow_settings' => true,
+  'allow_tests' => true,
+];
